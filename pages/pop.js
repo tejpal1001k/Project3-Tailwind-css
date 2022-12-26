3108a8a1f212a6ffdff9b06c69e3de83597e592c
Tejpal Choudhary <choudharytejpal148@gmail.com>
2022-12-26 15:46:33 +0530

    first commit

---
+++ pages/pop.js
@@ -0,0 +1,25 @@
+function closeForm() {
+    $('.form-popup-bg').removeClass('is-visible');
+  }
+  
+  $(document).ready(function($) {
+    
+    /* Contact Form Interactions */
+    $('#btnOpenForm').on('click', function(event) {
+      event.preventDefault();
+  
+      $('.form-popup-bg').addClass('is-visible');
+    });
+    
+      //close popup when clicking x or off popup
+    $('.form-popup-bg').on('click', function(event) {
+      if ($(event.target).is('.form-popup-bg') || $(event.target).is('#btnCloseForm')) {
+        event.preventDefault();
+        $(this).removeClass('is-visible');
+      }
+    });
+    
+    
+    
+    });
+  
